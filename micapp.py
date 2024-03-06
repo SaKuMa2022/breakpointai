c8ccaa9da8de66bf82b3de7c1abfa9b019a07cb6
SaKuMa2022 <114770923+SaKuMa2022@users.noreply.github.com>
2024-03-06 16:01:03 -0500

    Update micapp.py

--- micapp.py
+++ micapp.py
@@ -1,52 +1,40 @@
+
+import os
+from dotenv import load_dotenv
 import streamlit as st
 import pandas as pd
-
 from pandasai import SmartDataframe
 from pandasai.llm import OpenAI
-llm = OpenAI(api_token="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")
 
+load_dotenv()
+
+API_KEY = os.environ["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"]
+
+llm = OpenAI(api_token=API_KEY)
 
 import matplotlib.pyplot as plt
 from PIL import Image
-st.set_page_config(
-    page_title="AI assited app",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-styles = """
-<style>
-img {
-    max-width: 50%;
-}
-.sidebar .sidebar-content {
-    background-color: #f5f5f5;
-}
-</style>
-"""
+st.title("Prompt-driven analysis with OpenAI")
 
-st.sidebar.title('Datainsight')    
-st.sidebar.subheader('AI Insights')
-header = st.container()
 
-with header:
-    st.title("AI-Analysis App")
-    st.markdown("Use this  app to analyze your MIC data in one shot. You can upload your data and ask questions about it. The app will answer your questions and provide you with insights about your data.")
+uploaded_file = st.file_uploader("Upload a csv file to analysis", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df.head(5))
+     
+    prompt = st.text_area("Enter Your Prompt:")
     
-# Define main content
-content = st.container()
-with content:
-    # Load sales dataset
-    sale_file = st.file_uploader('Upload a CSV file', type=['csv'])
-    if sale_file is not None:
-        df = pd.read_csv(sale_file, encoding='latin-1')
-        sdf = SmartDataframe(df, config={"llm": llm})
-        st.dataframe(df)
-        query = st.text_input(label='Enter your query')
-        Analyze = st.button(label='Analyze')
-        if Analyze:
-            result = sdf.chat(query)
-            print(result)
-            st.write(result)
-    else:
-        st.warning("Please select a CSV file to continue.")
-        st.stop()
+    generate = st.button("Generate")
+    
+    if generate:
+        if prompt:
+            #st.write("OpenAI is generating an answer, please wait...")
+            with st.spinner("OpenAI is generating an answer, please wait..."):
+                #st.write(pandas_ai.run(df, prompt=prompt))
+                df = SmartDataframe(df, config={"llm": llm})
+                st.write(df.chat(prompt))
+            
+        else:
+            st.warning("Please upload a csv file and enter your prompt")
+
